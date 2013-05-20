a8759b8932594eb45f1331edeea0d406bd6106fd
holysin <furyng@gmail.com>
2013-05-20 16:42:58 +0800

    Initial Commit

---
+++ ZSTXL/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZSTXL
+//
+//  Created by LiuYue on 13-5-20.
+//  Copyright (c) 2013年 com.zxcxco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
