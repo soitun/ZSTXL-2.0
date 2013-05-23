3d471fa13c73bd6f70153671f8158dfbb389121a
holysin <furyng@gmail.com>
2013-05-23 19:13:08 +0800

    all contact myhomepage

---
+++ ZSTXL/Classes/Views/CityTitleView.h
@@ -0,0 +1,14 @@
+//
+//  cityTitleView.h
+//  ZSTXL
+//
+//  Created by LiuYue on 13-5-23.
+//  Copyright (c) 2013年 com.zxcxco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CityTitleView : UIView
+@property (retain, nonatomic) IBOutlet UILabel *cityLabel;
+
+@end
