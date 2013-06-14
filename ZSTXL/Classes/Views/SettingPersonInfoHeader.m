00797b30589381637e49839c42b30783cf6d58c1
holysin <furyng@gmail.com>
2013-06-14 11:27:01 +0800

    fix setting person info

---
+++ ZSTXL/Classes/Views/SettingPersonInfoHeader.m
@@ -0,0 +1,31 @@
+//
+//  SettingPersonInfoHeader.m
+//  ZSTXL
+//
+//  Created by LiuYue on 13-6-14.
+//  Copyright (c) 2013年 com.zxcxco. All rights reserved.
+//
+
+#import "SettingPersonInfoHeader.h"
+
+@implementation SettingPersonInfoHeader
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
